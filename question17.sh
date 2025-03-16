e433b87675b8975e2041016aa9884b5297874e9a
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-03-16 23:55:34 +1030

    Fix question17.sh

--- question17.sh
+++ question17.sh
@@ -1,1 +1,14 @@
-# Command sequence for question17
+git checkout -b branch1
+mv dir1/dir2/foo dir1/foo
+mv dir3/bar_copy dir3/bar
+rm -rf dir1/dir2
+touch newfile1
+git add .
+git commit -m "Setup branch1 structure"
+git checkout main
+git checkout -b branch2
+mv dir1/dir2/foo dir1/dir2/foo_modified
+mkdir dir1/dir3
+touch dir1/dir3/newfile2
+git add .
+git commit -m "Setup branch2 structure"
